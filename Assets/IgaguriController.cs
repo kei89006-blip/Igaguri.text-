43771e4a4b2b524b7807d9612fe4b3edf78054d6
keigo <kei89006@gmail.com>
2026-01-19 22:31:04 +0900

    igaguri shoot method

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)//どの方向に打ち出すのか？
+    {   // ↓この物体についている「物理エンジン（Rigidbody）」を見つけて       
+        GetComponent<Rigidbody>().AddForce(dir);// 指定された方向（dir）に「力（AddForce）」を加える
+    }
+    void Start()//ゲーム開始時　オブジェクトの作成　一回だけ
+    {
+        Application.targetFrameRate =60;//ゲームの滑らかさを秒間60フレームに固定
+        Shoot(new Vector3(0,200,2000));//引数（dir）には、(右に0, 上に200, 奥に2000) のベクトル
+    }
+
+
+}
