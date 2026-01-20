abac6feea4f158e2ebfbd7ff1ca2057c5cb83506
keigo <kei89006@gmail.com>
2026-01-20 18:44:00 +0900

    add iskinamatic ture

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class IgaguriController : MonoBehaviour
 {
     public void Shoot(Vector3 dir)//どの方向に打ち出すのか？
-    {   // ↓この物体についている「物理エンジン（Rigidbody）」を見つけて       
+    {//↓この物体についている「物理エンジン（Rigidbody）」を見つけて       
         GetComponent<Rigidbody>().AddForce(dir);// 指定された方向（dir）に「力（AddForce）」を加える
+    }
+
+    void OnConllisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
     }
     void Start()//ゲーム開始時　オブジェクトの作成　一回だけ
     {
